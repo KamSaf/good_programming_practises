dac16853745144da579c30eabe3e0d7995ec6263
Kamil <bopaf42@gmail.com>
2025-11-19 09:09:54 +0100

    [CU-86c6knh72] Alter models to be compatible with SQLAlchemy, delete logic from api endpoints

--- src/models.py
+++ src/models.py
@@ -1,30 +1,51 @@
-class Movie:
-    def __init__(self, movie_id: str, title: str, genres: str) -> None:
-        self.movie_id = int(movie_id)
-        self.title = title
-        self.genres = genres
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from typing import Optional
+from sqlalchemy import String, Integer, Float, ForeignKey
 
 
-class Link:
-    def __init__(self, movie_id: str, imdb_id: str, tmdb_id: str) -> None:
-        self.movie_id = int(movie_id)
-        self.imdb_id = int(imdb_id) if imdb_id else None
-        self.tmdb_id = int(tmdb_id) if tmdb_id else None
+class Base(DeclarativeBase):
+    pass
 
 
-class Rating:
-    def __init__(
-        self, user_id: str, movie_id: str, rating: str, timestamp: str
-    ) -> None:
-        self.user_id = int(user_id)
-        self.movie_id = int(movie_id)
-        self.rating = float(rating)
-        self.timestamp = int(timestamp)
+class Movie(Base):
+    __tablename__ = "movie"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(50))
+    genres: Mapped[str] = mapped_column(String(50))
+
+    def __repr__(self) -> str:
+        return (
+            f"Movie(movie_id={self.id!r}, title={self.title!r}, genres={self.genres!r})"
+        )
+
+
+class Link(Base):
+    __tablename__ = "link"
+    movie_id: Mapped[int] = mapped_column(ForeignKey("movie.id"))
+    imdb_id: Mapped[Optional[int]] = mapped_column(Integer, ForeignKey=True)
+    tmdb_id: Mapped[Optional[int]] = mapped_column(Integer, ForeignKey=True)
+
+    def __repr__(self) -> str:
+        return f"Link(movie_id={self.movie_id!r}, imdb_id={self.imdb_id!r}, tmdb_id={self.tmdb_id!r})"
+
+
+class Rating(Base):
+    __tablename__ = "rating"
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey=True)
+    movie_id: Mapped[int] = mapped_column(ForeignKey("movie.id"))
+    rating: Mapped[float] = mapped_column(Float)
+    timestamp: Mapped[int] = mapped_column(Integer)
+
+    def __repr__(self) -> str:
+        return f"Rating(user_id={self.user_id!r}, movie_id={self.movie_id!r}, rating={self.rating!r}, timestamp={self.timestamp!r})"
 
 
 class Tag:
-    def __init__(self, user_id: str, movie_id: str, tag: str, timestamp: str) -> None:
-        self.user_id = int(user_id)
-        self.movie_id = int(movie_id)
-        self.tag = tag
-        self.timestamp = int(timestamp)
+    __tablename__ = "tag"
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey=True)
+    movie_id: Mapped[int] = mapped_column(ForeignKey("movie.id"))
+    tag: Mapped[String] = mapped_column(String(50))
+    timestamp: Mapped[int] = mapped_column(Integer)
+
+    def __repr__(self) -> str:
+        return f"Tag(user_id={self.user_id!r}, movie_id={self.movie_id!r}, tag={self.tag!r}, timestamp={self.timestamp!r})"
--- src/routes.py
+++ src/routes.py
@@ -1,41 +1,23 @@
 from fastapi import APIRouter
-from src.models import Movie, Link, Rating, Tag
-from src.utils import read_csv
 
 router = APIRouter()
 
 
 @router.get("/movies")
 def get_movies() -> dict:
-    movies = [
-        Movie(movie_id, title, genres)
-        for [movie_id, title, genres] in read_csv("movies.csv")
-    ]
-    return {"movies": list(map(lambda m: m.__dict__, movies))}
+    return {"movies": "work in progress..."}
 
 
 @router.get("/links")
 def get_links() -> dict:
-    links = [
-        Link(movie_id, imdb_id, tmdb_id)
-        for [movie_id, imdb_id, tmdb_id] in read_csv("links.csv")
-    ]
-    return {"links": list(map(lambda m: m.__dict__, links))}
+    return {"links": "work in progress..."}
 
 
 @router.get("/ratings")
 def get_ratings() -> dict:
-    ratings = [
-        Rating(user_id, movie_id, rating, timestamp)
-        for [user_id, movie_id, rating, timestamp] in read_csv("ratings.csv")
-    ]
-    return {"ratings": list(map(lambda m: m.__dict__, ratings))}
+    return {"ratings": "work in progress..."}
 
 
 @router.get("/tags")
 def get_tags() -> dict:
-    tags = [
-        Tag(user_id, movie_id, tag, timestamp)
-        for [user_id, movie_id, tag, timestamp] in read_csv("tags.csv")
-    ]
-    return {"tags": list(map(lambda m: m.__dict__, tags))}
+    return {"tags": "work in progress..."}
